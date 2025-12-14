0c54fc4dd6f0736367ff269d1db514f1812e9f95
Yasin Quluzada <yasinquluzad850@gmail.com>
2025-12-14 23:23:27 +0400

    Delete 0-rectangle.py

--- 0-rectangle.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""This module defines a basic Rectangle class as a starting point for more features."""
-
-
-class Rectangle:
-    """Rectangle is an empty class that represents a rectangle object."""
-    pass
