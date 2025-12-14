9df75bacd136e76f690f71dcc66292c57e015221
yasinquluzada <yasinquluzad850@gmail.com>
2025-12-14 19:18:43 +0000

    Add empty Rectangle class

---
+++ 0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This module defines a basic Rectangle class as a starting point for more features."""
+
+
+class Rectangle:
+    """Rectangle is an empty class that represents a rectangle object."""
+    pass
